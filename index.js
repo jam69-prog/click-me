954f4405ea5e22e67496e2294318a6eb95e41a1b
jam69-prog <jamshiddiyor548@gmail.com>
2024-09-24 15:43:05 +0500

    click me

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+
+let btn = document.querySelector(".btn")
+let ball = document.querySelector(".ball")
+
+btn.onclik = () => {
+    ball.style.marginLeft = "100px"
+    ball.style.transition = "1s"
+}
